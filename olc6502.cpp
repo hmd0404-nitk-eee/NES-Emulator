304fb21cecd2794e4186941e8f614ab0814c4db1
gaurav2699 <gauravsingh2699@gmail.com>
2020-11-02 21:31:41 +0530

    Added 30 instructions

--- olc6502.cpp
+++ olc6502.cpp
@@ -59,6 +59,70 @@ void olc6502::clock()
 }
 
 
+void olc6502::reset(){
+  a=0;
+  x=0;
+  y=0;
+  stkp=0xFD;
+  status = 0x00 | U;
+  addr_abs = 0xFFFC;
+  uint16_t low = read(addr_abs+0);
+  uint16_t high = read(addr_abs+1);
+
+  pc = (high << 8)| low;
+  addr_rel = 0x0000;
+  addr_abs = 0x0000;
+  fetched = 0x00;
+  cycles =8;
+}
+
+
+void olc6502::irq()
+{
+  if (GetFlag(I) == 0){
+    write(0x0100 + stkp, (pc>>8)& 0x00FF);
+    stkp--;
+    write(0x0100 + stkp,  pc & 0x00FF);
+    stkp--;
+
+    SetFlag(B, 0);
+    SetFlag(U, 1);
+    SetFlag(I, 1);
+    write(0x0100 + stkp, status);
+    stkp--;
+
+    addr_abs = 0xFFFE;
+    uint16_t low = read(addr_abs+0);
+    uint16_t high = read(addr_abs+1);
+    pc = (high << 8)| low;
+
+    cycles = 7;
+  }
+}
+
+void olc6502::nmi()
+{
+
+    write(0x0100 + stkp, (pc>>8)& 0x00FF);
+    stkp--;
+    write(0x0100 + stkp,  pc & 0x00FF);
+    stkp--;
+
+    SetFlag(B, 0);
+    SetFlag(U, 1);
+    SetFlag(I, 1);
+    write(0x0100 + stkp, status);
+    stkp--;
+
+    addr_abs = 0xFFFA;
+    uint16_t low = read(addr_abs+0);
+    uint16_t high = read(addr_abs+1);
+    pc = (high << 8)| low;
+
+    cycles = 8;
+
+}
+
 uint8_t olc6502::GetFlag(FLAGS6502 f)
 {
 	return ((status & f) > 0) ? 1 : 0;
@@ -73,6 +137,7 @@ void olc6502::SetFlag(FLAGS6502 f, bool v)
 		status &= ~f;
 }
 
+// Addressing modes
 
 uint8_t olc6502::IMP(){
   fetched = a;
@@ -196,4 +261,336 @@ uint8_t olc6502::IZY(){
    if (addr_rel & 0x80){
      addr_rel |= 0xFF00;
    }
+
  }
+
+
+ // fetches the address
+ uint8_t olc6502::fetch(){
+   if(lookup[opcode].addrmode!= &olc6502::IMP){
+     fetched = read(addr_abs);
+   }
+   return fetched;
+ }
+
+ // Instruction codes
+ //1.
+// perform and operation
+ uint8t olc6502::ADD()
+ {
+   fetch();
+   a = a & fetched;
+   SetFlag(Z, a==0x00);
+   SetFlag(N, a & 0x80);
+   return 1;
+ }
+ //2.
+// Add the relative offset to the absolute address if carry flag is set
+ uint8_t olc6502:BCS(){
+   if(GetFlag(C)==1){
+     cycles++;
+     addr_abs = pc + addr_rel;
+     if((addr_abs & 0xFF00) != (pc & 0xFF00))
+        cycles++;
+     pc = addr_abs;
+   }
+   return 0;
+ }
+ //3.
+// Branch if zero bit is set
+ uint8_t olc6502:BEQ(){
+   if(GetFlag(Z)==1){
+     cycles++;
+     addr_abs = pc + addr_rel;
+     if((addr_abs & 0xFF00) != (pc & 0xFF00))
+        cycles++;
+     pc = addr_abs;
+   }
+   return 0;
+ }
+ //4.
+ // Branch if zero bit is not set
+ uint8_t olc6502:BNE(){
+    if(GetFlag(Z)==0){
+      cycles++;
+      addr_abs = pc + addr_rel;
+      if((addr_abs & 0xFF00) != (pc & 0xFF00))
+         cycles++;
+      pc = addr_abs;
+    }
+    return 0;
+  }
+ //5.
+  // Branch if minus bit is set
+   uint8_t olc6502:BMI(){
+     if(GetFlag(N)==1){
+       cycles++;
+       addr_abs = pc + addr_rel;
+       if((addr_abs & 0xFF00) != (pc & 0xFF00))
+          cycles++;
+       pc = addr_abs;
+     }
+     return 0;
+   }
+ //6.
+   // Branch if carry bit is clear
+  uint8_t olc6502:BCC(){
+    if(GetFlag(C)==0){
+      cycles++;
+      addr_abs = pc + addr_rel;
+      if((addr_abs & 0xFF00) != (pc & 0xFF00))
+         cycles++;
+      pc = addr_abs;
+    }
+    return 0;
+    }
+ //7.
+    // Branch if minus bit is clear
+ uint8_t olc6502:BPL(){
+    if(GetFlag(N)==0){
+       cycles++;
+     addr_abs = pc + addr_rel;
+     if((addr_abs & 0xFF00) != (pc & 0xFF00))
+        cycles++;
+     pc = addr_abs;
+   }
+   return 0;
+  }
+ //8.
+// Branch if overflow bit is clear
+uint8_t olc6502:BVC(){
+  if(GetFlag(V)==0){
+     cycles++;
+   addr_abs = pc + addr_rel;
+   if((addr_abs & 0xFF00) != (pc & 0xFF00))
+      cycles++;
+   pc = addr_abs;
+ }
+ return 0;
+}
+ //9.
+// Branch if overflow bit is set
+uint8_t olc6502:BVS(){
+  if(GetFlag(V)==1){
+     cycles++;
+   addr_abs = pc + addr_rel;
+   if((addr_abs & 0xFF00) != (pc & 0xFF00))
+      cycles++;
+   pc = addr_abs;
+ }
+ return 0;
+}
+
+ //10.
+// Clear the carry bit
+ uint8t olc6502::CLC()
+ {
+   SetFlag(C, false);
+   return 0;
+ }
+
+
+//11.
+ // Clear the interrupt bit
+  uint8t olc6502::CLI()
+  {
+    SetFlag(I, false);
+    return 0;
+  }
+
+//12.
+  // Clear the decimal bit
+   uint8t olc6502::CLD()
+   {
+     SetFlag(D, false);
+     return 0;
+   }
+
+//13.
+   // Clear the overflow flag
+    uint8t olc6502::CLV()
+    {
+      SetFlag(V, false);
+      return 0;
+    }
+//14.
+    // Set the carry flag
+    uint8t olc6502::SEC()
+    {
+      SetFlag(C, true);
+      return 0;
+    }
+
+//15.
+    // Set the decimal flag
+    uint8t olc6502::SED()
+    {
+      SetFlag(D, true);
+      return 0;
+    }
+//16.
+    // Set the interrupt flag
+    uint8t olc6502::SEI()
+    {
+      SetFlag(I, true);
+      return 0;
+    }
+
+//17.
+    //Addition with Carry
+    uint8t olc6502::ADC(){
+      fetch();
+      uint16_t sum = (uint16_t)a + (uint16_t)fetched + (uint16_t)GetFlag(C);
+      SetFlag(C, sum>255);
+      SetFlag(Z, (sum & 0x00FF) == 0);
+      SetFlag(N, sum&0x80);
+      SetFlag(V, (((uint16_t)a ^ (uint16_t)fetched) & ((uint16_t)a ^ (uint16_t)sum)) & 0x0080);
+      a = temp & 0x00FF;
+      return 1;
+    }
+//18.
+    //Subtraction with Carry
+    uint8t olc6502::SBC(){
+      fetch();
+      uint8_t value = ((uint16_t)fetched ^ 0x00FF)
+      uint16_t difference = (uint16_t)a + value + (uint16_t)GetFlag(C);
+      SetFlag(C, difference>255);
+      SetFlag(Z, (difference & 0x00FF) == 0);
+      SetFlag(N, difference&0x0080);
+      SetFlag(V, (((uint16_t)a ^ difference) & ((uint16_t)a ^ value)) & 0x0080);
+      a = temp & 0x00FF;
+      return 1;
+    }
+
+//19.
+    // Push the accumulator to the top of the Stack
+    uint8_t olc6502::PHA()
+    {
+      write(0x0100 + stkp, a);
+      stkp--;
+      return 0;
+    }
+
+//20.
+    // Pops the accumulator to the top of the Stack
+    uint8_t olc6502::PLA()
+    {
+      stkp++;
+      a = read(0x0100 + stkp);
+      SetFlag(Z, a == 0x00);
+      SetFlag(N, a & 0x80);
+      return 0;
+    }
+
+//21.
+    // Return from the interrupt state
+    uint8_t olc6502::RTI()
+    {
+      stkp++;
+      status = read(0x0100 + stkp);
+      status = status & ~B;
+      status = status & ~U;
+      stkp++;
+      pc = (uint16_t)read(0x0100 + stkp);
+      stkp++;
+      pc = pc | (uint16_t)read(0x0100 + stkp)<<8;
+      return 0;
+    }
+//22.
+    // Program sourced Interrupt
+    uint8_t olc6502::BRK(){
+      pc++;
+      write(0x0100 + stkp, pc>>8 & 0x00FF);
+      stkp--;
+      write(0x0100 + stkp, pc & 0x00FF);
+      stkp--;
+      SetFlag(B, 1);
+      write(0x0100 + stkp, status);
+      stkp--;
+      SetFlag(B, 0);
+      pc = (uint16_t)read(0xFFFE)|((uint16_t) read(0xFFFF) << 8);
+      return 0;
+
+    }
+
+    //23.
+// Arithmetic Left shift
+    uint8_t olc6502::ASL(){
+      fetch();
+      SetFlag(C, fetched & 0x80);
+      uint8_t temp = fetched << 1;
+      SetFlag(Z, temp==0x00);
+      SetFlag(N, temp & 0x80);
+      if (lookup[opcode].addrmode == &olc6502::IMP)
+		     a = (uint16_t) temp;
+	    else
+		    write(addr_abs, (uint16_t)temp);
+	    return 0;
+
+    }
+
+    //24.
+ // And bitwise operation
+    uint8_t olc6502::AND(){
+      fetch();
+      a = fetched & a;
+      SetFlag(Z, a==0x00);
+      SetFlag(N, a & 0x80);
+      return 1;
+    }
+
+  //25.
+// Test bits
+    uint8_t olc6502::BIT(){
+      fetch();
+      temp = a & fetched;
+      SetFlag(Z, (temp & 0x00FF)==0x00);
+      SetFlag(N, fetched & (1<<7) );
+      SetFlag(V, fetched & (1<<6) );
+      return 0;
+    }
+
+  //26.
+// Exclusive OR
+    uint8_t olc6502::EOR(){
+      fetch();
+      a = a ^ fetched;
+      SetFlag(Z, a==0x00);
+      SetFlag(N, a & 0x80);
+      return 1;
+    }
+
+  //27.
+// Decrement y
+    uint8_t olc6502::DEY(){
+      y--;
+      SetFlag(Z, y == 0x00);
+      SetFlag(N, y & 0x80);
+      return 0;
+    }
+
+  //28.
+// Decrement x
+    uint8_t olc6502::DEX()){
+      x--;
+      SetFlag(Z, x == 0x00);
+      SetFlag(N, x & 0x80);
+      return 0;
+    }
+  //29.
+// Increment y
+    uint8_t olc6502::INY(){
+      y++;
+      SetFlag(Z, y == 0x00);
+      SetFlag(N, y & 0x80);
+      return 0;
+    }
+
+    //30. 
+  // Increment x
+   uint8_t olc6502::INX()){
+      x++;
+      SetFlag(Z, x == 0x00);
+      SetFlag(N, x & 0x80);
+      return 0;
+    }
